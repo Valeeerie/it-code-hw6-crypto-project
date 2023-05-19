70a33a802203db4d3c7525d3326fcf5a5b9aff49
Ilikbaeva Valeriia <valeria180501@mail.ru>
2023-05-19 12:57:27 -0700

    исправила бесконечный скролл

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,93 +1,88 @@
 <template>
-    <div class="wrapper">
-        <div class="head">
-      <el-row :gutter="10">
-    <el-col :span="6"><div class="grid-content ep-bg-purple" />coin</el-col>
-    <el-col :span="6"><div class="grid-content ep-bg-purple" />symbol</el-col>
-    <el-col :span="6"><div class="grid-content ep-bg-purple" />current price</el-col>
-    <el-col :span="6"><div class="grid-content ep-bg-purple" />coin image</el-col>
-  </el-row>
-</div>
-<ul v-infinite-scroll="load" class="infinite-list" style="overflow: auto">
-    <li v-for="item in coins" :key="item" class="infinite-list-item">{{ item.title }}</li>
-  </ul>
-  <!-- <ul>
-    <li v-for="coin in coins">
-  <el-row :gutter="10">
-    <el-col :span="6"><div class="grid-content ep-bg-purple" />{{ coin.name }}</el-col>
-    <el-col :span="6"><div class="grid-content ep-bg-purple" />{{ coin.symbol }}</el-col>
-    <el-col :span="6"><div class="grid-content ep-bg-purple" />{{ coin.current_price }}</el-col>
-    <el-col :span="6"><div class="grid-content ep-bg-purple" /> <el-avatar :size="30" :src="coin.image" /></el-col>
-  </el-row>
-    </li>
-  </ul> -->
+  <div class="wrapper">
+    <div class="infinite-list-wrapper" style="overflow: auto">
+    <ul
+      v-infinite-scroll="load"
+      class="list"
+      :infinite-scroll-disabled="disabled"
+    >
+      <li v-for="coin in coins" :key="coin.id" class="list-item"> <el-avatar :size="30" :src="coin.image" />{{ coin.name }}</li>
+    </ul>
+    <p v-if="loading">Loading...</p>
+    <p v-if="noMore">No more</p>
+  </div>
 </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import makeRequest from '../utils/makeRequest';
 
 let coins= ref([])
-let initialValue=0
+let page=0
 
-
-makeRequest({                           
-  url: `https://fakestoreapi.com/products?limit=5`, 
+const loading = ref(false)
+const noMore = computed(() => coins.value.length >= 249)
+const disabled = computed(() => loading.value || noMore.value)
+const load = () => {
+  loading.value = true
+  setTimeout(() => {
+    makeRequest({                           
+    url: `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=${page}&sparkline=false&locale=en`, 
 }).then(({data}) => {                             
   for (let i=0;i<5;i++){
     coins.value.push(data[i])
   }
-  initialValue+=5
-  console.log(initialValue)
-  console.log(coins.value)
+  page++
 })
-
-function load(){
-makeRequest({                           
-  url: `https://fakestoreapi.com/products`, 
-}).then(({data}) => {                             
-  for (let i=initialValue;i<initialValue+5;i++){
-    coins.value.push(data[i])
-    initialValue++
-  }
-
-})
+    loading.value = false
+  }, 2000)
 }
 
+
+makeRequest({                           
+url: `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1&sparkline=false&locale=en`, 
+}).then(({data}) => {                             
+  coins.value=data
+  page++
+
+})
 
 </script>
 <style lang="scss" scoped>
 .wrapper{
-    margin-top: 2%;
-    gap:4%;
+  margin-top: 2%;
+  gap:4%;
 }
 .head{
-    margin-bottom: 10px;
+  margin-bottom: 10px;
 }
 
 ul{
 
 li{
-    list-style: none;
+  list-style: none;
 }}
 
-.infinite-list {
+.infinite-list-wrapper {
   height: 300px;
+  text-align: center;
+}
+.infinite-list-wrapper .list {
   padding: 0;
   margin: 0;
   list-style: none;
 }
-.infinite-list .infinite-list-item {
+
+.infinite-list-wrapper .list-item {
   display: flex;
   align-items: center;
   justify-content: center;
   height: 50px;
-  background: var(--el-color-primary-light-9);
-  margin: 10px;
-  color: var(--el-color-primary);
+  background: var(--el-color-danger-light-9);
+  color: var(--el-color-danger);
 }
-.infinite-list .infinite-list-item + .list-item {
+.infinite-list-wrapper .list-item + .list-item {
   margin-top: 10px;
 }
 </style>
